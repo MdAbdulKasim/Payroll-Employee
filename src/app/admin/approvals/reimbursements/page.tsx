7bb607a1837f77f69e0da7122a7e738d11dc595d
MohdHudhaifa03 <mohamedhudhaifazubair@gmail.com>
2025-12-27 17:24:24 +0530

    changes reimbursement page

--- src/app/admin/approvals/reimbursements/page.tsx
+++ src/app/admin/approvals/reimbursements/page.tsx
@@ -1,10 +1,10 @@
-import Reimbursements from "@/components/Admin/approvals/Reimbursements";
+import BenefitsManagementPage from "@/components/Admin/approvals/Reimbursements";
 import Layout from "@/components/Rootlayout/Layout";
 
 export default function ReimbursementsPage() {
   return (
     <Layout>
-      <Reimbursements />
+      <BenefitsManagementPage />
     </Layout>
   );
 }
